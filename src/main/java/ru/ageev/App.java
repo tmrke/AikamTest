5fff9185ea08ff52316086b8195a5e1167ba421f
tmrke <namat9@yandex.ru>
2023-09-29 00:56:01 +0700

    added result set

--- src/main/java/ru/ageev/App.java
+++ src/main/java/ru/ageev/App.java
@@ -3,10 +3,12 @@ package ru.ageev;
 
 import config.DatabaseConnection;
 import ru.ageev.criteria.Criteria;
+import ru.ageev.models.Customer;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class App {
@@ -17,6 +19,25 @@ public class App {
 
         List<Criteria> criteria = jsonParser.getCriteriaList(uri);
         Connection connection = DatabaseConnection.getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM customer WHERE last_name='Иванов'");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        List<Customer> customers = new ArrayList<>();
+
+        while (resultSet.next()){
+            Customer customer = new Customer();
+
+            customer.setId(resultSet.getInt("id"));
+            customer.setName(resultSet.getString("name"));
+            customer.setLastname(resultSet.getString("last_name"));
+
+            customers.add(customer);
+        }
+
+        System.out.println(customers.get(0).getLastname());
+
+        //List<Customer> customers = Collections.singletonList(preparedStatement.executeQuery().getObject("last_name", Customer.class));
     }
 }
 
--- src/main/java/ru/ageev/models/Customer.java
+++ src/main/java/ru/ageev/models/Customer.java
@@ -3,5 +3,30 @@ package ru.ageev.models;
 public class Customer {
     private int id;
     private String name;
-    private String lastName;
+    private String lastname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
 }
